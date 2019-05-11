a92676a5fbcab950a9a7e4d457a24dbb9f0da991
REZA AKIL <akil.reza@gmail.com>
2019-05-11 20:21:14 +0200

    2021

--- src/App.js
+++ src/App.js
@@ -1,68 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-
-
-const data = {
-  "Ivel Z3": {
-    manufacturer: "Ivasim",
-    year: 1969,
-    origin: "Croatia"
-  },
-  "Bally Astrocade": {
-    manufacturer: "Bally Consumer Products",
-    year: 1977,
-    origin: "USA"
-  },
-  "Sord M200 Smart Home Computer": {
-    manufacturer: "Sord Computer Corporation",
-    year: 1971,
-    origin: "Japan"
-  },
-  "Commodore 64": {
-    manufacturer: "Commodore",
-    year: 1982,
-    origin: "USA"
-  }
-}
-
-const formatData=(data) => data["Ivel Z3"].manufacturer+data["Bally Astrocade"].manufacturer+data["Sord M200 Smart Home Computer"].manufacturer 
-
-const element = (
-  <h1>
-    Hello data, {formatData(data)}!
-  </h1>
-);
-
-
-
-
-class Title extends Component {
-  
-  render() {
-
-    return (
-
-  
-    <>
-  <h1>{ this.props.content }</h1>
- </>
-
-    )
-
-
-  }
-}
-
+import PropTypes from 'prop-types'
 
 
 class App extends Component {
-  state = { active: true }
-
-  toggle = () => {
-    this.setState({
-      active: !this.state.active
-    })
-  }
   
   render(){
     
@@ -71,23 +12,85 @@ class App extends Component {
 
     <div className="App">
     <h1>this is home work</h1>
-      {element}
-      <Title content="Some Simple Title homework" />
-      <p>The light switch is <b>{ this.state.active ? 'on' : 'off' }</b></p>
-      <button onClick={this.toggle}>Toggle homework</button>
-      <select onClick="">
-    <option value="">-- pick a model --</option>
-    <option value="">Ivel</option>
-    <option value="Bally Astrocade">Bally Astrocade (1977)</option>
-    <option value="Sord M200 Smart Home Computer">Sord M200 Smart Home Computer (1971)</option>
-    <option value="Commodore 64">Commodore 64 (1982)</option>
-  </select>
+      {/* {element} */}
 
+      <select onClick="">
+        <option value="">-- pick a model --</option>
+        <option value="">Ivel</option>
+        <option value="Bally Astrocade">Bally Astrocade (1977)</option>
+        <option value="Sord M200 Smart Home Computer">Sord M200 Smart Home Computer (1971)</option>
+        <option value="Commodore 64">Commodore 64 (1982)</option>
+      </select>
+      <Title content="Some Simple Title" />
+      <LightSwitch />
+      <UserBoard/>
 
     </div>
   );
 
   }
 }
+
+class Title extends Component {
+  static propTypes = {
+    content: PropTypes.string.isRequired,
+  }
+  render() {
+    return (
+        <>
+        <h1>{ this.props.content }</h1>
+        </>
+    )
+  }
+}
+
+class LightSwitch extends Component {
+  state = { active: true }
+
+  toggle = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
+  render() {
+    return (<div>
+      <p>The light switch is <b>{ this.state.active ? 'on' : 'off' }</b></p>
+      <button onClick={this.toggle}>Toggle</button>
+    </div>)
+  }
+}
+
+class UserBoard extends Component {
+  render() {
+    return <div>
+          <User name="Chuck"  />
+          <User name="Bruce"  />
+          <User name="Arnold"  />
+          <User name="Sly"  />
+
+    </div>
+  }
+}
+class User extends Component {
+  state = { active: true }
+
+  toggle = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
+  render() {
+
+    return <div><p>User here: <b>{ this.props.name }</b></p>
+    <p>{ this.props.name } is <b>{ this.state.active ? 'Not' : '' }</b> present</p>
+    <button onClick={this.toggle}>Toggle</button>
+    </div>
+
+  }
+}
+
+
 
 export default App;
