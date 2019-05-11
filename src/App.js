fb681ccd7957297a90a838211e92346941596e5d
REZA AKIL <akil.reza@gmail.com>
2019-05-11 16:47:47 +0200

    1700

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// const user = {
-//   firstName: 'Harper',
-//   lastName: 'Perez',
-//   occupation:'student'
-// };
+
 const data = {
   "Ivel Z3": {
     manufacturer: "Ivasim",
@@ -37,10 +33,36 @@ const element = (
   </h1>
 );
 
-//console.log(data["Ivel Z3"])
+
+
+
+class Title extends Component {
+  
+  render() {
+
+    return (
+
+  
+    <>
+  <h1>{ this.props.content }</h1>
+ </>
+
+    )
+
+
+  }
+}
+
 
 
 class App extends Component {
+  state = { active: true }
+
+  toggle = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
   
   render(){
     
@@ -50,14 +72,17 @@ class App extends Component {
     <div className="App">
     <h1>this is home work</h1>
       {element}
+      <Title content="Some Simple Title homework" />
+      <p>The light switch is <b>{ this.state.active ? 'on' : 'off' }</b></p>
+      <button onClick={this.toggle}>Toggle homework</button>
+      <select onClick="">
+    <option value="">-- pick a model --</option>
+    <option value="">Ivel</option>
+    <option value="Bally Astrocade">Bally Astrocade (1977)</option>
+    <option value="Sord M200 Smart Home Computer">Sord M200 Smart Home Computer (1971)</option>
+    <option value="Commodore 64">Commodore 64 (1982)</option>
+  </select>
 
-    <select>
-      <option value="">-- pick a model --</option>
-      <option value="Ivel Z3">Ivel Z3 (1969)</option>
-      <option value="Bally Astrocade">Bally Astrocade (1977)</option>
-      <option value="Sord M200 Smart Home Computer">Sord M200 Smart Home Computer (1971)</option>
-      <option value="Commodore 64">Commodore 64 (1982)</option>
-    </select>
 
     </div>
   );
