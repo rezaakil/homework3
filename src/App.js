b436f37244716731e37e4fe55a1bc3be30fbba30
REZA AKIL <akil.reza@gmail.com>
2019-05-12 10:10:20 +0200

    rendering array of objects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types'
 
 
 class App extends Component {
@@ -10,7 +11,13 @@ class App extends Component {
 
 
     <div className="App">
+    <h1>this is home work</h1>
       {/* {element} */}
+
+      
+      <main>
+      <Scoreboard/>
+      </main>
       <SelectTag/>
 
     </div>
@@ -18,27 +25,72 @@ class App extends Component {
 
   }
 }
-
-class SelectTag extends Component {
-  state = { active: true }
-
-  optionClick = () => {
-    this.setState({
-      active: !this.state.active
-    })
+const players = [
+  {
+    id: 1,
+    name: 'Wouter',
+    score: 2
+  },
+  {
+    id: 2,
+    name: 'Mimi',
+    score: 5
+  },
+  {
+    id: 3,
+    name: 'Milan',
+    score: 4
   }
+]
 
+class Scoreboard extends Component {
+
+  renderPlayer(player) {
+    return <Player
+      name={player.name}
+      score={player.score}
+    />
+  }
 
   render() {
     return (
-      <select onClick={this.optionClick }>
+      <div className="scoreboard">
+        <h1>Scoreboard</h1>
+        <select onClick="">
+          {
+            players.map(this.renderPlayer)
+          }
+        </select>
+      </div>
+    )
+  }
+}
+
+class Player extends Component {
+  
+
+  render() {
+    return (
+      
+      
+        <option value={ this.props.score }> test{ this.props.name }</option>
+      
+    )
+  }
+}
+
+
+class SelectTag extends Component {
+  render() {
+    return (
+      <select onClick="">
        
         
-          <OptionTag name="-- pick a model --" value={this.state} />
-          <OptionTag name="Ivel" value={this.state} />
-          <OptionTag  name="Bally Astrocade (1977)" value={this.state} />
-          <OptionTag name="Sord M200 Smart Home Computer (1971)" value={this.state} />
-          <OptionTag name="Commodore 64 (1982)" value={this.state} />
+          <OptionTag name="-- pick a model --" value={2} />
+          <OptionTag name="Ivel" value={5} />
+          <OptionTag  name="Bally Astrocade (1977)" value={4} />
+          <OptionTag name="Sord M200 Smart Home Computer (1971)" value={2} />
+          <OptionTag name="Commodore 64 (1982)" value={5} />
          
 
         
@@ -58,14 +110,5 @@ class OptionTag extends Component {
     )
   }
 }
-
-
-
-
-
-
-
-
-
 
 export default App;
