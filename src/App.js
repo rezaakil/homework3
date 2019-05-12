6be5cbaa988786e27f7805749c1c9e9d517a8873
REZA AKIL <akil.reza@gmail.com>
2019-05-12 19:46:24 +0200

    local state

--- src/App.js
+++ src/App.js
@@ -1,151 +1,102 @@
 import React, { Component } from 'react';
 import './App.css';
-//import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 
 class App extends Component {
-  
   render(){
     
   return (
-
-
     <div className="App">
-    <h1>this is home work</h1>
-      {/* {element} */}
-
-      
-      <main>
-      <Scoreboard/>
-      </main>
-      <SelectTag/>
+  
+    <Select/>
 
     </div>
   );
 
   }
 }
-const players = [
-  {
-    key:1,
-    id: 1,
-    name: 'Wouter',
-    score: 2
-  },
-  {
-    key:2,
-    id: 2,
-    name: 'Mimi',
-    score: 5
-  },
-  {
-    key:3,
-    id: 3,
-    name: 'Milan',
-    score: 4
-  },
-  {
-    key:4,
-    id: 3,
-    name: 'Milan',
-    score: 4
-  }
-]
 
-
-const playerst = {
-  "Ivel Z3": {
-    manufacturer: "Ivasim",
-    year: 1969,
-    origin: "Croatia"
-  },
-  "Bally Astrocade": {
-    manufacturer: "Bally Consumer Products",
-    year: 1977,
-    origin: "USA"
-  },
-  "Sord M200 Smart Home Computer": {
-    manufacturer: "Sord Computer Corporation",
-    year: 1971,
-    origin: "Japan"
-  },
-  "Commodore 64": {
-    manufacturer: "Commodore",
-    year: 1982,
-    origin: "USA"
-  }
-}
-//const breeds = Object.keys(player.name)
-
-class Scoreboard extends Component {
-
-  renderPlayer(player) {
-    return <Player
-      key={Object.values(player.key)}
-
-      name={Object.values(player.name)}
-      data={Object.values(player)}
-    />
-  }
-  
-
-  render() {
-    return (
-      <div className="scoreboard">
-        <h1>Scoreboard</h1>
-        <select>
-          {
-            players.map(this.renderPlayer)
-          }
-        </select>
-      </div>
-    )
-  }
+class Select extends Component {
+  state = {
+    data:[
+    {
+      id: 1,
+      name: "Ivel Z3",
+      manufacturer:  "Ivasim",
+      year: 1969,
+      origin: "Croatia"
+    },
+    {
+      id: 2,
+      name: "Bally Astrocade",
+      manufacturer:"Bally Consumer Products",
+      year: 1977,
+      origin: "USA",
+    },
+    {
+      id: 3,
+      name: "Sord M200 Smart Home Computer",
+      manufacturer:"Sord Computer Corporation",
+      year: 1971,
+      origin: "Japan"
+    },
+    {
+      id: 4,
+      name: "Commodore 64",
+      manufacturer:  "Commodore",
+      year: 1982,
+      origin: "USA"
+    }
+  ]
 }
 
-class Player extends Component {
-  
+  renderComputer = (computer) => {
+  return <Option
+    id={computer.id}
+    name={computer.name}
+    key={computer.id}
+    year={computer.year}
+  />
+}
 
-  render() {
-    return (
-      
-      
-        <option value={ this.props.data }>{ this.props.name }</option>
-      
-    )
-  }
+updateSelection(event) {
+  this.setState(this.state.data.name);
 }
 
 
-class SelectTag extends Component {
-  render() {
-    return (
-      <select onClick="">
-       
-        
-          <OptionTag name="-- pick a model --" value={2} />
-          <OptionTag name="Ivel" value={5} />
-          <OptionTag  name="Bally Astrocade (1977)" value={4} />
-          <OptionTag name="Sord M200 Smart Home Computer (1971)" value={2} />
-          <OptionTag name="Commodore 64 (1982)" value={5} />
-         
-
-        
-
+render = (computer) => {
+  return (
+    <div>
+      
+      <select value={this.state.data.name} onChange={this.updateSelection} >
+      <option value="">-- pick a model --</option>
+        {
+          this.state.data
+          .map(this.renderComputer)
+        }
       </select>
-    )
+    </div>
+  )
+}
+}
+
+class Option extends Component {
+  static propTypes = {
+      name: PropTypes.string.isRequired
+  }
+
+  handleOnClick = () => {
+  
+  }
+  
+render = () => {
+  return (
+    <option value={this.props.name} >{ this.props.name } ({ this.props.year })
+    </option>
+  )
   }
 }
 
-
-class OptionTag extends Component {
- 
-
-  render() {
-    return (
-      <option value="1">{ this.props.name }</option>
-    )
-  }
-}
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { createStore } from 'redux'
-import reducer from './reducer'
+
+// We change this line:
+import reducer from './reducers' // which is shorthand for import ./reducers/index.js
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
